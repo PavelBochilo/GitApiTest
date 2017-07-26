8c4cbcbf2354d46062b4ca1999879ee3cc52d9ad
Pavel Bochilo <Pavel_Bochilo@epam.com>
2017-07-26 14:50:53 +0300

    Initial Commit

---
+++ GitApiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitApiTest
+//
+//  Created by Pavel Bochilo on 26.07.17.
+//  Copyright © 2017 Pavel Bochilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitApiTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitApiTest
+//
+//  Created by Pavel Bochilo on 26.07.17.
+//  Copyright © 2017 Pavel Bochilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
